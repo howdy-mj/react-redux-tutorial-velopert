f3951f6c8d493061b286e0d1603a06e76706211b
kmj <hi.minjungkim@gmail.com>
2020-09-08 22:45:43 +0900

    에러 수정

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -37,23 +37,25 @@ const initialState = {
 };
 
 // reducer
-const todos = handleActions({
-  [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
-  [INSERT]: (state, { payload: todo }) => ({
-    ...state,
-    todos: state.todos.concat(todo),
-  }),
-  [TOGGLE]: (state, { payload: id }) => ({
-    ...state,
-    todos: state.todos.map((todo) =>
-      todo.id !== id ? { ...todo, done: !todo.done } : todo,
-    ),
-  }),
-  [REMOVE]: (state, { payload: id }) => ({
-    ...state,
-    todos: state.todos.filter((todo) => todo.id !== id),
-  }),
+const todos = handleActions(
+  {
+    [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
+    [INSERT]: (state, { payload: todo }) => ({
+      ...state,
+      todos: state.todos.concat(todo),
+    }),
+    [TOGGLE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.map((todo) =>
+        todo.id !== id ? { ...todo, done: !todo.done } : todo,
+      ),
+    }),
+    [REMOVE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.filter((todo) => todo.id !== id),
+    }),
+  },
   initialState,
-});
+);
 
 export default todos;
