4e31f3045cedc9cc4a4f9ef034595f8166ca01f9
kmj <hi.minjungkim@gmail.com>
2020-09-08 22:21:22 +0900

    redux-actions로 더 편하게 사용하기

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 벨로퍼트 '리액트를 다루는 기술'
 
 17장: 리덕스를 사용하여 리액트 애플리케이션 상태 관리하기
+
+사용 패키지
+
+- redux
+- react-redux
+- redux-devtools-extension
+- redux-actions
--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,10 +1,16 @@
+import { createAction, handleActions } from 'redux-actions';
+
 // action
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
 // action creator function
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+// export const increase = ({ type: INCREASE })
+// export const decrease = ({ type: DECREASE })
+
+// 액션 객체를 넣을 필요 없이 선언 가능
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 // initialState
 const initialState = {
@@ -12,19 +18,26 @@ const initialState = {
 };
 
 // reducer
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+);
+// function counter(state = initialState, action) {
+//   switch (action.type) {
+//     case INCREASE:
+//       return {
+//         number: state.number + 1,
+//       };
+//     case DECREASE:
+//       return {
+//         number: state.number - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// }
 
 export default counter;
